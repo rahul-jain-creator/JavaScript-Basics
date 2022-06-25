dd516e153701e8f83cd89ab18b43eb2c00767863
Rahul Kumar Jain <rahul_kumarjain@yahoo.com>
2022-06-25 19:36:32 +0530

    Approved

---
+++ Function_getIndexValue.js
@@ -0,0 +1,7 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a' 
+// Index Starting from 1 ( Practical , Starting from Zero )
+
+function myFunc(a,n){
+    return a[n-1];
+}
---
+++ Function_strictlyequal.js
@@ -0,0 +1,6 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunc(a,b){
+    return a===b;
+}
