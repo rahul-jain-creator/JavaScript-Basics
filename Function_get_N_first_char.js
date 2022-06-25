a0d68235beb08e3c2c534645affdfedcc6876fc2
Rahul Kumar Jain <rahul_kumarjain@yahoo.com>
2022-06-25 20:45:17 +0530

    Approved

---
+++ Function_get_N_first_char.js
@@ -0,0 +1,6 @@
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+function myFunc(a){
+    return a.slice(0,3);
+}
---
+++ Function_remove_N_char.js
@@ -0,0 +1,6 @@
+// Write a function that takes a string (a) as argument
+// Remove the first 3 characters of a
+// Return the result
+function myFunc(a){
+    return a.slice(3);
+}
