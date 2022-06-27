30cbfbfe1790e5ddcf724c643989ffd1d7446e29
Rahul Kumar Jain <rahul_kumarjain@yahoo.com>
2022-06-27 19:07:29 +0530

    Approved

---
+++ 12.Function_Is_even.js
@@ -0,0 +1,12 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+function myFunc(a){
+    if(a%2==0){
+        return true; }else{ 
+            false;
+        }
+    }
+
+console.log(myFunc(10));
